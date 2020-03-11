d6ef34ef9e26881f69620f806207d8f79466d323
Ivan Lemeshev <vanlmshv@gmail.com>
2020-03-12 02:28:16 +0300

    Add basic logic and response

--- web/index.php
+++ web/index.php
@@ -2,4 +2,35 @@
 
 require_once dirname(__DIR__) .'/vendor/autoload.php';
 
-echo 'hello from index';
+use Dotenv\Dotenv;
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+use App\Service\Supermetrics;
+
+$env = Dotenv::createImmutable(__DIR__ . '/../');
+$env->load();
+
+$logger = new Logger('supermetrics-app');
+$logger->pushHandler(new StreamHandler(__DIR__.'/../logs/supermetrics-app.log', Logger::DEBUG));
+$logger->info('test');
+
+
+$supermetrics = new Supermetrics($_ENV['SUPERMETRICS_BASE_URL'], $logger);
+$token = $supermetrics->register($_ENV['SUPERMETRICS_CLIENT_ID'], $_ENV['SUPERMETRICS_EMAIL'], $_ENV['SUPERMETRICS_NAME']);
+
+$result = [];
+
+if ($token !== '') {
+    for ($page = 1; $page < 11; $page++) {
+        $posts = $supermetrics->getPosts($token, $page);
+        $result = array_merge($result, $posts);
+    }
+}
+
+// TODO Average character length of a post / month
+// TODO Longest post by character length / month
+// TODO Total posts split by week
+// TODO Average number of posts per user / month
+
+header('Content-Type: application/json');
+echo json_encode($result);
