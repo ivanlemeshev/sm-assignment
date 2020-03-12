f11acfe158975642d2a94ec6e6ad3b5538b99be9
Ivan Lemeshev <vanlmshv@gmail.com>
2020-03-13 01:01:05 +0300

    Add post entity test

--- src/Entity/Post.php
+++ src/Entity/Post.php
@@ -49,29 +49,14 @@ class Post
      */
     public function __construct(array $data = [])
     {
-        if (!empty($data['id']) && is_string($data['id'])) {
-            $this->id = $data['id'];
-        }
 
-        if (!empty($data['from_name']) && is_string($data['from_name'])) {
-            $this->fromName = $data['from_name'];
-        }
-
-        if (!empty($data['from_id']) && is_string($data['from_id'])) {
-            $this->fromId = $data['from_id'];
-        }
-
-        if (!empty($data['message']) && is_string($data['message'])) {
-            $this->message = $data['message'];
-        }
-
-        if (!empty($data['type']) && is_string($data['type'])) {
-            $this->message = $data['type'];
-        }
-
-        if (!empty($data['created_time']) && is_string($data['created_time'])) {
-            $this->createdTime = new DateTime($data['created_time']);
-        }
+        // TODO validate values
+        $this->id = $data['id'];
+        $this->fromName = $data['from_name'];
+        $this->fromId = $data['from_id'];
+        $this->message = $data['message'];
+        $this->type = $data['type'];
+        $this->createdTime = new DateTime($data['created_time']);
     }
 
     /**
---
+++ tests/unit/Entity/PostTest.php
@@ -0,0 +1,55 @@
+<?php
+
+use App\Entity\Post;
+use Codeception\Test\Unit;
+
+class PostTest extends Unit
+{
+    protected Post $post;
+
+    protected function _before()
+    {
+        try {
+            $this->post = new Post([
+                'id' => 'post5e68609a29ea7_6eeaa679',
+                'from_name' => 'Nydia Croff',
+                "from_id" => "user_2",
+                'message' => 'post_1',
+                'type' => 'status',
+                'created_time' => '2020-03-11T00:34:23+00:00',
+            ]);
+        } catch (\Exception $e) {
+            // TODO handle this case
+        }
+    }
+
+    public function testGetId()
+    {
+        $this->assertEquals('post5e68609a29ea7_6eeaa679', $this->post->getId());
+    }
+
+    public function testFromName()
+    {
+        $this->assertEquals('Nydia Croff', $this->post->getFromName());
+    }
+
+    public function testFromId()
+    {
+        $this->assertEquals('user_2', $this->post->getFromId());
+    }
+
+    public function testGetMessage()
+    {
+        $this->assertEquals('post_1', $this->post->getMessage());
+    }
+
+    public function testGetType()
+    {
+        $this->assertEquals('status', $this->post->getType());
+    }
+
+    public function testCreatedTime()
+    {
+        $this->assertEquals(new DateTime('2020-03-11T00:34:23+00:00'), $this->post->getCreatedTime());
+    }
+}
