59e2de8186e3eeda7cdfa2e4d66c0fe274ef71bd
Ivan Lemeshev <vanlmshv@gmail.com>
2020-03-13 02:27:41 +0300

    Create search statistics

---
+++ src/Handler/Statistics.php
@@ -0,0 +1,181 @@
+<?php
+
+namespace App\Handler;
+
+use App\Entity\Post;
+
+/**
+ * Class Statistics
+ *
+ * @package App\Handler
+ */
+class Statistics
+{
+    /**
+     * @var Post[]
+     */
+    private array $posts = [];
+
+    /**
+     * @var int
+     */
+    private int $postsCount = 0;
+
+    /**
+     * PostStatistics constructor.
+     *
+     * @param array $posts
+     */
+    public function __construct(array $posts)
+    {
+        $this->posts = $posts;
+        $this->postsCount = count($posts);
+    }
+
+    /**
+     * @return array
+     */
+    public function show(): array
+    {
+        return [
+            'post_average_length_by_month' => $this->getPostAverageLengthByMonth(),
+            'longest_post_by_month' => $this->getLongestPostByMonth(),
+            'total_posts_by_week' => $this->getTotalPostsByWeek(),
+            'average_number_of_user_posts_by_month' => $this->getAverageNumberOfUserPostsByMonth(),
+        ];
+    }
+
+    /**
+     * @return array
+     */
+    private function getPostAverageLengthByMonth(): array
+    {
+        if ($this->postsCount == 0) {
+            return [];
+        }
+
+        $totalLength = [];
+        $postCounts = [];
+
+        foreach ($this->posts as $post) {
+            $month = $post->getCreatedTime()->format('m.Y');
+
+            if (!isset($totalLength[$month])) {
+                $totalLength[$month] = 0;
+                $postCounts[$month] = 0;
+            }
+
+            $totalLength[$month] += mb_strlen($post->getMessage());
+            $postCounts[$month]++;
+        }
+
+        $result = [];
+        foreach ($totalLength as $month => $total) {
+            $result[$month] = floor($total / $postCounts[$month] * 100) / 100;
+        }
+
+        return $result;
+    }
+
+    /**
+     * @return array
+     */
+    private function getLongestPostByMonth(): array
+    {
+        if ($this->postsCount == 0) {
+            return [];
+        }
+
+        $longestPosts = [];
+        foreach($this->posts as $post) {
+            $month = $post->getCreatedTime()->format('m.Y');
+            $length = mb_strlen($post->getMessage());
+
+            if (!isset($longestPosts[$month])) {
+                $longestPosts[$month] = 0;
+            }
+
+            if ($length > $longestPosts[$month]) {
+                $longestPosts[$month] = $length;
+            }
+        }
+
+        $result = [];
+        foreach ($longestPosts as $month => $length) {
+            $result[$month] = $length;
+        }
+
+        return $result;
+    }
+
+    /**
+     * @return array
+     */
+    private function getTotalPostsByWeek(): array
+    {
+        if ($this->postsCount == 0) {
+            return [];
+        }
+
+        $totalPosts = [];
+        foreach ($this->posts as $post) {
+            $dateTime = clone $post->getCreatedTime();
+            $weekStartDate = $dateTime->modify('Monday this week');
+
+            $key = $weekStartDate->format('d.m.Y');
+            if (!isset($totalPosts[$key])) {
+                $totalPosts[$key] = 0;
+            }
+
+            $totalPosts[$key]++;
+        }
+
+        uksort($totalPosts, function($a, $b) {
+            $tm1 = strtotime($a);
+            $tm2 = strtotime($b);
+            return ($tm1 < $tm2) ? 1 : (($tm1 > $tm2) ? -1 : 0);
+        });
+
+        return $totalPosts;
+    }
+
+    /**
+     * @return array
+     */
+    private function getAverageNumberOfUserPostsByMonth(): array {
+        if ($this->postsCount == 0) {
+            return [];
+        }
+
+        $monthPosts = [];
+        foreach ($this->posts as $post) {
+            $month = $post->getCreatedTime()->format('m.Y');
+            $user = $post->getFromId();
+
+            if (!isset($monthPosts[$month])) {
+                $monthPosts[$month] = [];
+            }
+
+            if (!isset($monthPosts[$month][$user])) {
+                $monthPosts[$month][$user] = 0;
+            }
+
+            $monthPosts[$month][$user]++;
+        }
+
+        $result = [];
+        foreach ($monthPosts as $month => $users) {
+            $totalUsers = 0;
+            $totalPosts = 0;
+
+            foreach ($users as $user => $count) {
+                $totalUsers++;
+                $totalPosts += $count;
+            }
+
+            $result[$month] = floor($totalPosts / $totalUsers * 100) / 100;
+        }
+
+        return $result;
+    }
+}
---
+++ tests/unit/Handler/StatisticsTest.php
@@ -0,0 +1,128 @@
+<?php
+
+use App\Handler\Statistics;
+use App\Entity\Post;
+use Codeception\Test\Unit;
+
+class StatisticsTest extends Unit
+{
+    public function testShow()
+    {
+        $posts = [];
+
+        $expected = [
+            'post_average_length_by_month' => [],
+            'longest_post_by_month' => [],
+            'total_posts_by_week' => [],
+            'average_number_of_user_posts_by_month' => [],
+        ];
+
+        $statistics = new Statistics($posts);
+        $this->assertEquals($expected, $statistics->show());
+
+        $posts = [
+            new Post([
+                'id' => '10',
+                'from_name' => 'Author Name 2',
+                "from_id" => "author_2",
+                'message' => 'post_10',
+                'type' => 'status',
+                'created_time' => '2020-02-05T00:00:01+00:00',
+            ]),
+            new Post([
+                'id' => '9',
+                'from_name' => 'Author Name 1',
+                "from_id" => "author_1",
+                'message' => 'post_9',
+                'type' => 'status',
+                'created_time' => '2020-02-04T00:00:01+00:00',
+            ]),
+            new Post([
+                'id' => '8',
+                'from_name' => 'Author Name 1',
+                "from_id" => "author_1",
+                'message' => 'post_8',
+                'type' => 'status',
+                'created_time' => '2020-02-03T00:00:01+00:00',
+            ]),
+            new Post([
+                'id' => '7',
+                'from_name' => 'Author Name 2',
+                "from_id" => "author_2",
+                'message' => 'post_7',
+                'type' => 'status',
+                'created_time' => '2020-02-02T00:00:01+00:00',
+            ]),
+            new Post([
+                'id' => '6',
+                'from_name' => 'Author Name 1',
+                "from_id" => "author_1",
+                'message' => 'post_6',
+                'type' => 'status',
+                'created_time' => '2020-02-01T00:00:01+00:00',
+            ]),
+            new Post([
+                'id' => '5',
+                'from_name' => 'Author Name 1',
+                "from_id" => "author_1",
+                'message' => 'post_5',
+                'type' => 'status',
+                'created_time' => '2020-01-31T00:00:01+00:00',
+            ]),
+            new Post([
+                'id' => '4',
+                'from_name' => 'Author Name 1',
+                "from_id" => "author_2",
+                'message' => 'post_4',
+                'type' => 'status',
+                'created_time' => '2020-01-30T00:00:01+00:00',
+            ]),
+            new Post([
+                'id' => '3',
+                'from_name' => 'Author Name 2',
+                "from_id" => "author_2",
+                'message' => 'post_3',
+                'type' => 'status',
+                'created_time' => '2020-01-29T00:00:01+00:00',
+            ]),
+            new Post([
+                'id' => '2',
+                'from_name' => 'Author Name 1',
+                "from_id" => "author_1",
+                'message' => 'post_2',
+                'type' => 'status',
+                'created_time' => '2020-01-28T00:00:01+00:00',
+            ]),
+            new Post([
+                'id' => '1',
+                'from_name' => 'Author Name 1',
+                "from_id" => "author_1",
+                'message' => 'post_1',
+                'type' => 'status',
+                'created_time' => '2020-01-27T03:00:00+00:00',
+            ]),
+        ];
+
+        $expected = [
+            'post_average_length_by_month' => [
+                '02.2020' => 6.2,
+                '01.2020' => 6.0,
+            ],
+            'longest_post_by_month' => [
+                '02.2020' => 7,
+                '01.2020' => 6,
+            ],
+            'total_posts_by_week' => [
+                '03.02.2020' => 3,
+                '27.01.2020' => 7,
+            ],
+            'average_number_of_user_posts_by_month' => [
+                '02.2020' => 2.5,
+                '01.2020' => 2.5,
+            ],
+        ];
+
+        $statistics = new Statistics($posts);
+        $this->assertEquals($expected, $statistics->show());
+    }
+}
