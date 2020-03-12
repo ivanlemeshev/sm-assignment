69bad5ea9ff0fdac82fe19f09c2f4b509c6e75e2
Ivan Lemeshev <vanlmshv@gmail.com>
2020-03-13 02:28:08 +0300

    Implement reponse handler

--- web/index.php
+++ web/index.php
@@ -2,35 +2,27 @@
 
 require_once dirname(__DIR__) .'/vendor/autoload.php';
 
+use App\Handler\Statistics;
+use App\Provider\SupermetricsPosts;
+use App\Service\Supermetrics\Client;
+use App\Service\Supermetrics\Credentials;
 use Dotenv\Dotenv;
 use Monolog\Logger;
 use Monolog\Handler\StreamHandler;
-use App\Service\Supermetrics;
+
+session_start();
+
+header('Content-Type: application/json');
 
 $env = Dotenv::createImmutable(__DIR__ . '/../');
 $env->load();
 
 $logger = new Logger('supermetrics-app');
 $logger->pushHandler(new StreamHandler(__DIR__.'/../logs/supermetrics-app.log', Logger::DEBUG));
-$logger->info('test');
 
+$supermetrics = new Client($_ENV['SUPERMETRICS_BASE_URL'], $logger);
+$credentials = new Credentials($_ENV['SUPERMETRICS_CLIENT_ID'], $_ENV['SUPERMETRICS_EMAIL'], $_ENV['SUPERMETRICS_NAME']);
+$posts = new SupermetricsPosts($supermetrics, $credentials);
+$statistics = new Statistics($posts->getPosts());
 
-$supermetrics = new Supermetrics($_ENV['SUPERMETRICS_BASE_URL'], $logger);
-$token = $supermetrics->register($_ENV['SUPERMETRICS_CLIENT_ID'], $_ENV['SUPERMETRICS_EMAIL'], $_ENV['SUPERMETRICS_NAME']);
-
-$result = [];
-
-if ($token !== '') {
-    for ($page = 1; $page < 11; $page++) {
-        $posts = $supermetrics->getPosts($token, $page);
-        $result = array_merge($result, $posts);
-    }
-}
-
-// TODO Average character length of a post / month
-// TODO Longest post by character length / month
-// TODO Total posts split by week
-// TODO Average number of posts per user / month
-
-header('Content-Type: application/json');
-echo json_encode($result);
+echo json_encode($statistics->show());
